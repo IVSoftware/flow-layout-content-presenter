d802faab3d4ce04daad80918da0eaa700cffa498
Tom Gregor <tgregor@ivsoftware.com>
2025-02-24 03:54:59 -0700

    FSOL: Test Options

--- Pagination/Main Form.Designer.cs
+++ Pagination/Main Form.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             contentPresenterFlowLayout = new Pagination.Presentation.ContentPresenterFlowLayout();
+            menuStrip = new MenuStrip();
+            testToolStripMenuItem = new ToolStripMenuItem();
+            itemsToolStripMenuItem = new ToolStripMenuItem();
+            itemsToolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            imageWithRandoControlsToolStripMenuItem = new ToolStripMenuItem();
+            regularButtonsToolStripMenuItem = new ToolStripMenuItem();
+            irregularButtonsToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // contentPresenterFlowLayout
@@ -36,11 +45,70 @@
             contentPresenterFlowLayout.BackColor = SystemColors.Control;
             contentPresenterFlowLayout.Dock = DockStyle.Fill;
             contentPresenterFlowLayout.Font = new Font("Segoe UI", 12F);
-            contentPresenterFlowLayout.Location = new Point(2, 2);
+            contentPresenterFlowLayout.Location = new Point(2, 42);
             contentPresenterFlowLayout.Name = "contentPresenterFlowLayout";
             contentPresenterFlowLayout.PageSize = 10;
-            contentPresenterFlowLayout.Size = new Size(1074, 540);
+            contentPresenterFlowLayout.Size = new Size(1074, 500);
             contentPresenterFlowLayout.TabIndex = 1;
+            // 
+            // menuStrip
+            // 
+            menuStrip.Font = new Font("Segoe UI", 14F);
+            menuStrip.ImageScalingSize = new Size(24, 24);
+            menuStrip.Items.AddRange(new ToolStripItem[] { testToolStripMenuItem });
+            menuStrip.Location = new Point(2, 2);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(1074, 40);
+            menuStrip.TabIndex = 2;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            testToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { itemsToolStripMenuItem, itemsToolStripMenuItem1, toolStripMenuItem1, imageWithRandoControlsToolStripMenuItem, regularButtonsToolStripMenuItem, irregularButtonsToolStripMenuItem });
+            testToolStripMenuItem.Font = new Font("Segoe UI", 12F);
+            testToolStripMenuItem.Name = "testToolStripMenuItem";
+            testToolStripMenuItem.Size = new Size(72, 36);
+            testToolStripMenuItem.Text = "Test";
+            // 
+            // itemsToolStripMenuItem
+            // 
+            itemsToolStripMenuItem.Name = "itemsToolStripMenuItem";
+            itemsToolStripMenuItem.Size = new Size(433, 40);
+            itemsToolStripMenuItem.Text = "25 Items";
+            itemsToolStripMenuItem.Tag = TestOption.Items25;
+            // 
+            // itemsToolStripMenuItem1
+            // 
+            itemsToolStripMenuItem1.Name = "itemsToolStripMenuItem1";
+            itemsToolStripMenuItem1.Size = new Size(433, 40);
+            itemsToolStripMenuItem1.Text = "1M Items";
+            itemsToolStripMenuItem1.Tag = TestOption.Items1M;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(430, 6);
+            // 
+            // imageWithRandoControlsToolStripMenuItem
+            // 
+            imageWithRandoControlsToolStripMenuItem.Name = "imageWithRandoControlsToolStripMenuItem";
+            imageWithRandoControlsToolStripMenuItem.Size = new Size(433, 40);
+            imageWithRandoControlsToolStripMenuItem.Text = "Image With Random Controls";
+            imageWithRandoControlsToolStripMenuItem.Tag = TestOption.ImageRando;
+            // 
+            // regularButtonsToolStripMenuItem
+            // 
+            regularButtonsToolStripMenuItem.Name = "regularButtonsToolStripMenuItem";
+            regularButtonsToolStripMenuItem.Size = new Size(433, 40);
+            regularButtonsToolStripMenuItem.Text = "Regular Buttons";
+            regularButtonsToolStripMenuItem.Tag = TestOption.ButtonRegular;
+            // 
+            // irregularButtonsToolStripMenuItem
+            // 
+            irregularButtonsToolStripMenuItem.Name = "irregularButtonsToolStripMenuItem";
+            irregularButtonsToolStripMenuItem.Size = new Size(433, 40);
+            irregularButtonsToolStripMenuItem.Text = "Irregular Buttons";
+            irregularButtonsToolStripMenuItem.Tag = TestOption.ButtonIrregular;
             // 
             // MainForm
             // 
@@ -49,16 +117,29 @@
             BackColor = Color.Aqua;
             ClientSize = new Size(1078, 544);
             Controls.Add(contentPresenterFlowLayout);
+            Controls.Add(menuStrip);
             Font = new Font("Segoe UI", 10F);
+            MainMenuStrip = menuStrip;
             Name = "MainForm";
             Padding = new Padding(2);
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Main Form";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Pagination.Presentation.ContentPresenterFlowLayout contentPresenterFlowLayout;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem testToolStripMenuItem;
+        private ToolStripMenuItem itemsToolStripMenuItem;
+        private ToolStripMenuItem itemsToolStripMenuItem1;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem imageWithRandoControlsToolStripMenuItem;
+        private ToolStripMenuItem regularButtonsToolStripMenuItem;
+        private ToolStripMenuItem irregularButtonsToolStripMenuItem;
     }
 }
--- Pagination/Main Form.cs
+++ Pagination/Main Form.cs
@@ -2,6 +2,7 @@ using Pagination.Presentation;
 
 namespace Pagination
 {
+    enum TestOption { Items25, Items1M, ImageRando, ButtonRegular, ButtonIrregular }
     public partial class MainForm : Form
     {
         // https://picsum.photos/
@@ -13,6 +14,34 @@ namespace Pagination
 
             contentPresenterFlowLayout.Items.AddRange(
                 Enumerable.Range(0, NUMBER_OF_ITEMS).Select(_=>new ContentPresenterViewModel(testData: true)));
+
+            testToolStripMenuItem.DropDownItemClicked += (sender, e) =>
+            {
+                if (e.ClickedItem?.Tag is TestOption option)
+                {
+                    switch (option)
+                    {
+                        case TestOption.Items25:
+                            contentPresenterFlowLayout.Items.Clear();
+                            contentPresenterFlowLayout.Items.AddRange(
+                                Enumerable.Range(0, 25).Select(_ => new ContentPresenterViewModel(testData: true)));
+                            break;
+                        case TestOption.Items1M:
+                            contentPresenterFlowLayout.Items.Clear();
+                            contentPresenterFlowLayout.Items.AddRange(
+                                Enumerable.Range(0, 1000000).Select(_ => new ContentPresenterViewModel(testData: true)));
+                            break;
+                        case TestOption.ImageRando:
+                            break;
+                        case TestOption.ButtonRegular:
+                            break;
+                        case TestOption.ButtonIrregular:
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            };
         }
     }
 }
